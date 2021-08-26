b96e72d095fdf87cbe05be89dedbd4a944c08c6a
sakshikamal <sakshikamal99@gmail.com>
2021-08-26 16:24:01 +0530

    new trained model saved

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from google.oauth2 import service_account
 from flask import Flask, render_template, session, redirect
 from functools import wraps
 import pymongo
+import sys,errno
 
 
 
@@ -56,11 +57,11 @@ for i in values:
   p=p+1
 # print(values)
 
-app = Flask(__name__)
-# app = Flask(__name__,
-#             static_url_path='', 
-#             static_folder='static',
-#             template_folder='templates')
+# app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # # Database
@@ -128,6 +129,7 @@ def dashboard():
   import datetime
   from sklearn import metrics
   import pickle
+  import numpy as np
   #loc='csvfiles/House'+house+'.csv'
   # loc='csvfiles/House5.csv'
   # print(loc)
@@ -163,64 +165,84 @@ def dashboard():
   df1=pd.DataFrame(values)
   df1.columns = df1.iloc[0]
   df1 = df1.iloc[1:]
-  #df1['Datetime']=pd.to_datetime(df1['Dateee'] + ' ' + df1['Time'])
+  df1['Datetime']=pd.to_datetime(df1['Date'] + ' ' + df1['Time'])
   #df1['Datetime']=df1['Dateee']+' '+ df1['Time']
   #df1['Datetime']=pd.to_datetime(df1['Date'], errors='ignore')
-  df1['Datetime']=df1['Date'].astype('datetime64[ns]')
-  df1['Air Quality'] = df1['Air Quality'].apply(pd.to_numeric, errors='coerce')
-  df1['Turbidity'] = df1['Turbidity'].apply(pd.to_numeric, errors='coerce')
+  # df1['Datetime']=df1['Date'].astype('datetime64[ns]')
+  df1['Air Quality(PPM)'] = df1['Air Quality(PPM)'].apply(pd.to_numeric, errors='coerce')
+  df1['Turbidity(NTU)'] = df1['Turbidity(NTU)'].apply(pd.to_numeric, errors='coerce')
   df1= df1.dropna()
   print("last values")
   real_time_val=df1.tail(1)
   temp=real_time_val.iloc[0]['Temperature(C)']
-  humid=real_time_val.iloc[0]['Humidity']
-  air=real_time_val.iloc[0]['Air Quality']
-  turb=real_time_val.iloc[0]['Turbidity']
+  humid=real_time_val.iloc[0]['Humidity(%)']
+  air=real_time_val.iloc[0]['Air Quality(PPM)']
+  turb=real_time_val.iloc[0]['Turbidity(NTU)']
   print(df1.info())
 
-  df1= df1[['Datetime', 'Air Quality', 'Turbidity']]
+  #future dates array
+  # X1_future=np.array(pd.to_datetime(['2021-05-03T00:00:00.000000000','2021-05-04T00:00:00.000000000','2021-05-05T00:00:00.000000000','2021-05-06T00:00:00.000000000','2021-05-07T00:00:00.000000000','2021-05-08T00:00:00.000000000','2021-05-08T00:00:00.000000000','2021-05-09T00:00:00.000000000']))
+  X1_future=np.array(pd.to_datetime(['2021-05-03','2021-05-04','2021-05-05','2021-05-06','2021-05-07','2021-05-08','2021-05-08']))
+  print(type(X1_future[0]))
+  print(X1_future)
+  X1_future = X1_future.astype("datetime64[ns]")
+  #X1_future=np.array(pd.date_range(start="2021-05-03",end="2021-05-09").to_pydatetime().tolist())
+  #X1_future=np.array(['2021-05-03T00:00:00.000000000','2021-05-04T00:00:00.000000000','2021-05-05T00:00:00.000000000','2021-05-06T00:00:00.000000000','2021-05-07T00:00:00.000000000','2021-05-08T00:00:00.000000000','2021-05-08T00:00:00.000000000'])
+  df1= df1[['Datetime', 'Air Quality(PPM)', 'Turbidity(NTU)']]
+  #df1= df1[[ 'Air Quality', 'Turbidity']]
   print(df1)
 
 
   X = df1['Datetime'].values
-  y = df1['Air Quality'].values
+  y = df1['Air Quality(PPM)'].values
   from sklearn.model_selection import train_test_split
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05)
 
   # # Fitting Random Forest Regression to the dataset
   # #air quality
-  # from sklearn.ensemble import RandomForestRegressor
-  # model = RandomForestRegressor(n_estimators = 10, random_state = 0)
-  # model.fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
+  from sklearn.ensemble import RandomForestRegressor
+  model = RandomForestRegressor(n_estimators = 1000, random_state = 0)
+  model.fit(X_train.reshape(-1,1), y_train.reshape(-1,1))
   
+
+  ##printing whats in X_test
+  # print("X_TESTTTTTTTTTT")
+  # print(X_test)
   # #save the model
-  filename_air = 'finalized_model_air.sav'
-  # pickle.dump(model, open(filename_air, 'wb'))
+  
   # y_pred = model.predict(X_test.reshape(-1,1))
   # y_pred.reshape(-1)
-  # # print(y_pred)
+  # print("AIR TRAINING THEN PLOTTING")
+  # print(y_pred)
+  # filename_air = 'saved_model_air.sav'
+  # pickle.dump(model, open(filename_air, 'wb'))
 
-  #check how model performs
-  loaded_model = pickle.load(open(filename_air, 'rb'))
+  #check how saved model performs
+  # filename_air = 'finalized_model_air.sav'
+  # loaded_model = pickle.load(open(filename_air, 'rb'))
+
   #result = loaded_model.score(X_test, y_test)
   #print(result)
+  filename_air = 'saved_model_air.sav'
+  loaded_model = pickle.load(open(filename_air, 'rb'))
   y_pred = loaded_model.predict(X_test.reshape(-1,1))
+  print("LOADED MODEL")
   y_pred.reshape(-1)
   print(y_pred)
 
-  import numpy as np
+  
   import matplotlib.pyplot as plt
 
-  # plt.scatter(X_test, y_test, color = 'red')
-  # plt.scatter(X_test, y_pred, color = 'green')
-  # plt.title('Random Forest Regression')
-  # plt.xlabel('Time')
-  # plt.ylabel('Sensor Values')
-  # plt.show()
+  plt.scatter(X_test, y_test, color = 'red')
+  plt.scatter(X_test, y_pred, color = 'green')
+  plt.title('Random Forest Regression')
+  plt.xlabel('Time')
+  plt.ylabel('Air Sensor Values')
+  plt.show()
 
-  print(y_pred)
-
-  print(y_test)
+  # print(y_pred)
+  # print("Y TESTTTTTTTTTTTTTTTTTTTTT")
+  # print(y_test)
   print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
   print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
   print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
@@ -235,26 +257,31 @@ def dashboard():
 
   #water
   X1 = df1['Datetime'].values
-  y1 = df1['Turbidity'].values
+  y1 = df1['Turbidity(NTU)'].values
   from sklearn.model_selection import train_test_split
   X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size = 0.05)
 
   # Fitting Random Forest Regression to the dataset
   from sklearn.ensemble import RandomForestRegressor
-  model = RandomForestRegressor(n_estimators = 10, random_state = 0)
+  model = RandomForestRegressor(n_estimators = 1000, random_state = 0)
   model.fit(X1_train.reshape(-1,1), y1_train.reshape(-1,1))
 
-  filename_water = 'finalized_model_water.sav'
-  #pickle.dump(model, open(filename_water, 'wb'))
+  # filename_water = 'saved_model_water.sav'
+  # pickle.dump(model, open(filename_water, 'wb'))
+  # y1_pred = model.predict(X1_test.reshape(-1,1))
+  # y1_pred.reshape(-1)
+  # print("WATER TRAINING THEN PLOTTING")
+  # print(y1_pred)
 
+  filename_water = 'finalized_model_water.sav'
   loaded_model = pickle.load(open(filename_water, 'rb'))
-  y1_pred =loaded_model.predict(X1_test.reshape(-1,1))
+  y1_pred = loaded_model.predict(X1_test.reshape(-1,1))
   y1_pred.reshape(-1)
   print(y1_pred)
 
   #print(y1_pred)
 
-  print(y1_test)
+  #print(y1_test)
 
   day1=y1_pred[0]
   day2=y1_pred[1]
@@ -265,9 +292,9 @@ def dashboard():
   day7=y1_pred[6]
   week1=[day1,day2,day3,day4,day5,day6,day7]
   import matplotlib.pyplot as pyplot
-  days = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
-  #pyplot.plot(days, week)
-  #pyplot.show()
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  pyplot.plot(days, week)
+  pyplot.show()
  
 
   exp_bill=[week[0],week[1],week[2],week[3],week[4],week[5],week[6]]
